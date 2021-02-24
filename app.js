41a533c0082abaead2b520358956a980dc269198
Bogdan-create <bogdan-shigaev@mail.ru>
2021-02-24 17:44:35 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const dbURL =
+  'mongodb+srv://bakhteev:323694m@cluster0.ss8ji.mongodb.net/auth-project?retryWrites=true&w=majority'
+
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+mongoose
+  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((results) => app.listen(3001))
+  .catch((err) => console.log(err))
+
+app.get('/', (req, res) => res.render('pages/index'))
