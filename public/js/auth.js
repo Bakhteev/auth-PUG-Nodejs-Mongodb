266a184874529d365143b6a8818ad4ceb7087b0f
Bogdan-create <bogdan-shigaev@mail.ru>
2021-02-25 13:12:51 +0600

    almost finished login && signup

---
+++ public/js/auth.js
@@ -0,0 +1,26 @@
+const labels = document.querySelectorAll('.label')
+const inputs = document.querySelectorAll('.label__input')
+
+const handleChange = (e) => {
+  if (e.target.value !== '') {
+    e.target.classList.add('active')
+    labels.forEach((label) => {
+      if (e.target.id === label.getAttribute('for')) {
+        label.classList.add('active')
+      }
+    })
+  } else {
+    e.target.classList.remove('active')
+    labels.forEach((label) => {
+      if (e.target.id === label.getAttribute('for')) {
+        label.classList.remove('active')
+      }
+    })
+  }
+}
+inputs.forEach((input) => {
+  input.addEventListener('input', handleChange)
+})
+inputs.forEach((input) => {
+  input.addEventListener('change', handleChange)
+})
